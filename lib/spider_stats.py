d2970d9900c8c71ea6d0569dc715aa8d2fc5ad17
arnald <arnald.alonso@gmail.com>
2016-12-04 12:42:06 +0100

    star-fusion

--- lib/spider_stats.py
+++ lib/spider_stats.py
@@ -105,7 +105,9 @@ def stats_trimgalore(path):
                     r2 = data[1][i]
                 g1 = g1 + "\t" + r1
                 g2 = g2 + "\t" + r2
+                print r2
                 if '%' in r2:
+                    print ' ' + r2.split(' ')[1].replace('(', '').replace('%)', '')
                     plt1 = plt1 + "\t" + r1.split(' ')[1].replace('(', '').replace('%)', '')
                     plt2 = plt2 + "\t" + r2.split(' ')[1].replace('(', '').replace('%)', '')
             else:
