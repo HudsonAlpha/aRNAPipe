14ebb3d93037b7dd12dfd54007c2cba7731d84e5
arnald <arnald.alonso@gmail.com>
2016-12-04 12:51:36 +0100

    star-fusion

--- spider.py
+++ spider.py
@@ -121,8 +121,9 @@ try:
         print "  - " + path + "/HTML/trim.html"
         html_table = html.print_table_default(path + "/outputs/stats_trim.txt", -1, []) # PROVIDES HTML TABLE WITH HPC STATS
         data = html.bar_getdata(path + "/outputs/stats_trim_plot.txt",0,[],[])
-        print data
-        html.build_from_template("TrimGalore", project, data, html_table, "", path+"/HTML/trim.html", os.path.dirname(sys.argv[0]) + "/template/TEMPLATE_TRIMG.html", lmenu)
+#        html.build_from_template("TrimGalore", project, data, html_table, "", path+"/HTML/trim.html", os.path.dirname(sys.argv[0]) + "/template/TEMPLATE_TRIMG.html", lmenu)
+
+        html.build_amcharts(os.path.dirname(sys.argv[0]) + "/template/TEMPLATE_TRIMG.html", path + "/HTML/trim.html", 'TrimGalore', ['TrimGalore', 'TrimGalore'], path, html_table, project, lmenu)
 except:
     print "  - Not ready"
 
