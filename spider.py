60dc32ec22f13c1315314e8e7d852c1d25aad866
arnald <arnald.alonso@gmail.com>
2016-12-08 08:30:53 +0100

    jsplice

--- spider.py
+++ spider.py
@@ -264,7 +264,7 @@ except:
 if config.has_key("jsplice"):
     print "> Generating webpage with picard statistics..."
     print "  - " + path + "/HTML/jsplice.html"
-    html_table = html.print_table_default(path + "/results_jsplice/", -1, []) # PROVIDES HTML TABLE WITH HPC STATS
+    html_table = html.print_table_default(path + "/results_jsplice/jSplice_results.txt", -1, []) # PROVIDES HTML TABLE WITH HPC STATS
     html.build_from_template("jSplice", project, "", html_table, "", path+"/HTML/picard.html", os.path.dirname(sys.argv[0]) + "/template/TEMPLATE_JSPLICE.html", lmenu)
 #except:
 #    print "  - Not ready"
